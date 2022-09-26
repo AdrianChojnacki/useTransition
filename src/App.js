bc32077c68b5d06fb9405454145bb87c670524ca
Adrian Chojnacki <achojnacki@pgs-soft.com>
2022-09-26 17:01:11 +0200

    useTransition

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
-import { useState } from "react";
+import { useState, useTransition } from "react";
 
 function App() {
   const [input, setInput] = useState("");
   const [list, setList] = useState([]);
+  const [isPending, startTransition] = useTransition();
 
   const LIST_SIZE = 20000;
 
   const handleChange = e => {
     setInput(e.target.value);
 
-    const l = [];
-
-    for (let i = 0; i < LIST_SIZE; i++) {
-      l.push(e.target.value);
-    }
-
-    setList(l);
+    startTransition(() => {
+      const l = [];
+      for (let i = 0; i < LIST_SIZE; i++) {
+        l.push(e.target.value);
+      }
+      setList(l);
+    });
   };
 
   return (
     <>
       <input type="text" value={input} onChange={handleChange} />
-      {list.map((item, index) => {
-        return <div key={index}>{item}</div>;
-      })}
+      {isPending ? (
+        <div>Loading...</div>
+      ) : (
+        list.map((item, index) => {
+          return <div key={index}>{item}</div>;
+        })
+      )}
     </>
   );
 }
